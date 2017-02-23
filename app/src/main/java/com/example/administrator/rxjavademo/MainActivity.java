5b04d34fe6576fc43780a26437cbd862eb343a2a
andy <andygo520@163.com>
2017-02-23 15:00:39 +0800

    use RxJava2.0 with Retrofit2.0

--- app/src/main/java/com/example/administrator/rxjavademo/MainActivity.java
+++ app/src/main/java/com/example/administrator/rxjavademo/MainActivity.java
@@ -3,95 +3,105 @@ package com.example.administrator.rxjavademo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "RxJavaDemo";
-    private final String[] names = {"Jack", "Tom", "Lucy", ""};
+    private  String baseUrl = "https://api.douban.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Observable observable = Observable.create(new ObservableOnSubscribe() {
-//            @Override
-//            public void subscribe(ObservableEmitter e) throws Exception {
-//                e.onNext(1);
-//                e.onNext(2);
-//                e.onNext(3);
-//                e.onNext(4);
-//                e.onComplete();
-//            }
-//        }).subscribeOn(Schedulers.io())//通过Scheduler切换线程,subscribeOn指定在io线程发送事件
-//                .observeOn(AndroidSchedulers.mainThread());//observeOn指定在主线程接收事件
-
-//        Observable observable=Observable.fromArray("Jack","Tom","Lucy","");//用just、from等运算符同样可以像create一样快捷创建事件队列
-//        Observer<String> observer = new Observer<String>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
-//                Log.d(TAG, "onSubscribe");
-//            }
-//
-//            @Override
-//            public void onNext(String value) {
-//                Log.d(TAG, value);
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.d(TAG, "onError");
-//
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                Log.d(TAG, "onComplete");
-//            }
-//        };
-
-//        observable.subscribe(observer);
-
-        Observable.range(310, 11)//从310开始，发送11个数据元素
-                .filter(new Predicate<Integer>() {
+        final TextView textView = (TextView) findViewById(R.id.tv);
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(new OkHttpClient())
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                //Retrofit暂不支持RxJava2.0，需要引入'com.jakewharton.retrofit:retrofit2-rxjava2-adapter:1.0.0'
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+        DouBanApi api = retrofit.create(DouBanApi.class);
+        api.getBook(1764796)//传入电影id得到发送Movie事件的observable
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Movie>() {
                     @Override
-                    public boolean test(Integer integer) throws Exception {
-                        return integer != 315;
-                    }
-                })//去除315（false）
-                .takeLast(5) //只选取最后5个元素
-                .map(new Function<Integer, String>() {
-
-                    @Override
-                    public String apply(Integer integer) throws Exception {
-                        return integer + "00";
-                    }
-                })//将发送Integer数据的Observable转换为发送String类型数据的Observable
-                .subscribe(new Observer<String>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(String value) {
-                        Log.d(TAG, value);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
+                    public void accept(Movie Movie) throws Exception {
+                        textView.setText(Movie.getTitle());
                     }
                 });
+    }
+
+    public void merge() {
+        List<String> list = new ArrayList<>();
+        List<String> list1 = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            list.add("hello" + i);
+        }
+        for (int j = 0; j < 20; j++) {
+            list.add("world" + j);
+        }
+
+        Observable<List<String>> hello = Observable.fromArray(list);
+        Observable<List<String>> world = Observable.fromArray(list1);
+
+        Observable.merge(hello, world).subscribe(new Observer<List<String>>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(List<String> value) {
+                for (String string : value) {
+                    Log.d(TAG, string);
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+    }
+
+    public void subscribe() {
+        Observable observable = Observable.create(new ObservableOnSubscribe() {
+            @Override
+            public void subscribe(ObservableEmitter e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onNext(4);
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.io())//通过Scheduler切换线程,subscribeOn指定在io线程发送事件
+                .observeOn(AndroidSchedulers.mainThread());//observeOn指定在主线程接收事件
     }
 }
