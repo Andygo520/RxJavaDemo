ae29bc95397862099827d8219d9c6bc49f4a8574
andy <andygo520@163.com>
2017-02-23 15:05:38 +0800

    use RxJava2.0 with Retrofit2.0

---
+++ app/src/main/java/com/example/administrator/rxjavademo/DouBanApi.java
@@ -0,0 +1,14 @@
+package com.example.administrator.rxjavademo;
+
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by Administrator on 2017/2/16.
+ */
+public interface DouBanApi {
+     @GET("v2/movie/subject/{id}")
+     public Observable<Movie> getBook(@Path("id") int id);
+}
